42e8657e1e5ac3593e0f0201db0490102477d16f
Eetu Asseri <eetuasseri@gmail.com>
2024-10-29 11:22:23 +0200

    Update 12.2.WeatherAPI.py

--- HarjoitusTehtavia/Tehtavat 12/12.2.WeatherAPI.py
+++ HarjoitusTehtavia/Tehtavat 12/12.2.WeatherAPI.py
@@ -1,8 +1,36 @@
 import json
-import os
 import requests
 
-api_key = json.loads('weatherApiKey.json')["apiKey"]
+'''
+Make a file with the name "weatherApiKey.json" in the same folder as this script.
+The contents of said file should match the example below, with 'KEY_HERE' being your API key.
+{
+  "apiKey" : "KEY_HERE"
+}
+'''
 
-geocodeapi = f"http://api.openweathermap.org/geo/1.0/zip?zip={zipcode},{country_code}&limit=1&appid={api_key}"
-weatherapi = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}"
+with open('weatherApiKey.json') as f:
+    api_key = json.load(f)["apiKey"]
+
+def kelvin_to_celcius(kelvin):
+    return round(kelvin - 273.15, 2)
+
+while True:
+    city_name = input("Paikkakunnan nimi: ")
+    if city_name == "":
+        break
+    geocodeapi = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={api_key}"
+    georesponce = requests.get(geocodeapi).json()[0]
+    lat = georesponce["lat"]
+    lon = georesponce["lon"]
+    country = georesponce["country"]
+    name = georesponce["name"]
+    weatherapi = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+    weatherresponce = requests.get(weatherapi).json()
+    weather = weatherresponce["weather"][0]
+    main = weatherresponce["main"]
+    weather_type = weather["main"]
+    weather_desc = weather["description"]
+    temperature = main["temp"]
+
+    print(f"{name} in {country} has a {weather_desc} with a temperature of {kelvin_to_celcius(temperature)} celcius.")
